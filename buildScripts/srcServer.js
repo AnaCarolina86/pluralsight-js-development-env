7d5119436a63efaf767926e938d214aef0c73f35
Ana Carolina Mendes <anaccm86@gmail.com>
2020-04-20 18:04:26 +0200

    automation, transpiling and bundling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,23 @@
+//I can change here, using import express
+// jshint esversion: 6
+
 const express = require('express');
 const path = require('path');
 const open = require('open');
 
+import  webpack  from 'webpack';
+import config from '../webpack.config.dev';
+
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+//it is to integrate Express with webpack
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+
+}));
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
