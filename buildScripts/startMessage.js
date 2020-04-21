ebf49903652bea4ded8ced467fda47556718d189
Ana Carolina Mendes <anaccm86@gmail.com>
2020-04-21 13:21:27 +0200

    Linting

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -2,4 +2,6 @@
 
 import chalk from 'chalk';
 
-console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
+
+//this comment is useful for exceptions 
